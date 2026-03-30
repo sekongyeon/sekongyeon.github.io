062a4afc4158dae7996e3c1a5b86b726725e06e5
sekongyeon <107653787+sekongyeon@users.noreply.github.com>
2026-03-30 21:27:49 +0900

    Add files via upload

---
+++ max.php
@@ -0,0 +1,26 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <form method="get" action="max.php">
+        input number : <input type="text" name="count">
+        <input type="submit" value="ok!">
+    </form>
+
+    <?php
+    if (isset($_GET['count']) && $_GET['count'] !== "") {
+    $count = (int)$_GET['count'];
+    $nums = [];
+
+    for ($i = 0; $i < $count; $i++) {
+    $nums[] = rand(1, 100);
+    }
+
+    echo implode(" ", $nums) . "<br>";
+    echo "maximum = " . max($nums);
+    }
+    ?>
+</body>
+</html>
---
+++ sum.php
@@ -0,0 +1,36 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Sum Result</title>
+</head>
+<body>
+    <form method="get" action="sum.php">
+        input number : <input type="text" name="num" value="<?php echo isset($_GET['num']) ? $_GET['num'] : ''; ?>">
+        <input type="submit" value="ok!">
+    </form>
+
+    <?php
+    if (isset($_GET['num']) && $_GET['num'] !== "") {
+        $n = (int)$_GET['num'];
+        $even = []; 
+        $even_sum = 0;
+        $odd = []; 
+        $odd_sum = 0;
+
+        for ($i = 0; $i < $n - 1; $i++) {
+            if ($i % 2 == 0) {
+                $even[] = $i;
+                $even_sum += $i;
+            } else {
+                $odd[] = $i;
+                $odd_sum += $i;
+            }
+        }
+
+        echo "<br>sum of even number less than $n :" . implode("+", $even) . "=$even_sum <br>";
+        echo "sum of odd number less than $n :" . implode("+", $odd) . "=$odd_sum";
+    } 
+    ?>
+</body>
+</html>
